adceca7ee97d652a98c305702c19409194c721b1
Jack Davis <jack.davis@akqa.com>
2017-03-29 13:25:13 -0700

    Header module

---
+++ src/app/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './header.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>My Contentful Example Site</h1>
+    </div>
+  )
+};
+
+export default Header;
---
+++ src/app/main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Header from './components/header'
+
+export default class Main extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
