2d974c9dc32afe6809936583524f6a6c20c46ca0
Jack Davis <jack.davis@akqa.com>
2017-04-19 16:55:38 -0700

    Progressive JPGs

--- src/app/components/post-tile/post-tile.js
+++ src/app/components/post-tile/post-tile.js
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './post-tile.scss';
 
 const PostTile = props => {
+  const imgUrl = props.post.fields.mainImage.fields.file.url + '?fm=jpg&fl=progressive';
+
   return (
     <div className={styles.post}>
       <h2 className={styles.postTitle}>{props.post.fields.title}</h2>
       <p className={styles.postText}>{props.post.fields.bodyText}</p>
-      <img className={styles.postImage} src={props.post.fields.mainImage.fields.file.url} />
+      <img className={styles.postImage} src={imgUrl} />
       <h3 className={styles.postCategory}>{props.post.fields.category.fields.title}</h3>
     </div>
   )
